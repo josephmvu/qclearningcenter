9e4e1a8b59c7bdbe84e6c81e9127aa9ccfa138ed
Joseph Vu <hello@josephvu.me>
2016-11-03 19:25:36 +0000

    add passport and set routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var path = require('path'),
     logger = require('morgan'),
     express = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    session = require('express-session'),
+    flash = require('connect-flash');
     
 var indexRoutes = require('./routes/index');
+var userRoutes = require('./routes/users');
 
 var app = express();
 
@@ -12,10 +16,27 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// setup passport
+app.use(session({
+    secret: 'this is not a good secret',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport');
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 app.use(indexRoutes);
+app.use(userRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
